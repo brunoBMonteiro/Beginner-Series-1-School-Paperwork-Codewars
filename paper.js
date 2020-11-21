54e1564353d6ff7e3d7490acbeb288eefdf94ac6
Bruno Bilheri <52968377+brunoBMonteiro@users.noreply.github.com>
2020-11-21 09:58:47 -0300

    Solution finalized

---
+++ paper.js
@@ -0,0 +1,25 @@
+//Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+//Your task is to calculate how many blank pages do you need.
+
+//Example:
+//paperwork(5, 5) == 25
+//Note: if n < 0 or m < 0 return 0!
+
+//Waiting for translations and Feedback! Thanks!
+
+//function paperwork(n, m) {
+  
+//}
+
+
+//solution
+
+function paperwork(n, m) {
+    if (n > 0  && m > 0){
+      return n*m
+    }
+    else{
+      return 0;
+    }
+  }
